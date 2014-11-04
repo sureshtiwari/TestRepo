7c87e4fc3f6dbdacd345c0be4035dbbdc63d738a
Suresh Tiwari <suresh.tiwari03091993>
2014-11-04 23:40:08 +0530

    added ignore

--- testgit/src/com/suresh/Testclass.java
+++ testgit/src/com/suresh/Testclass.java
@@ -4,7 +4,7 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("it is the first change");
+		System.out.println("it is the first change") ;
 		// TODO Auto-generated method stub
 
 	}
